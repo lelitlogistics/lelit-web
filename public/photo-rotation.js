e4705a2356758038d01e52221d63f2f93b9621b6
lelitlogistics <hiring@lelitlogistics.com>
2026-09-22 17:01:44 -0400

    Use unique Northeast-friendly photos across rotating sections

--- public/photo-rotation.js
+++ public/photo-rotation.js
@@ -3,32 +3,34 @@
   const INTERVAL = 10000;
   const FADE = 1400;
 
+  // Every URL below is unique across the entire page. The set favors
+  // Northeast / New England visual context and intentionally avoids LA/palms.
   const galleries = [
     {
       selector: '.hero__media img',
       delay: 0,
       images: [
-        'https://assets.aboutamazon.com/dims4/default/fa084b1/2147483647/strip/true/crop/2000x1334%2B0%2B0/resize/1114x743%21/quality/90/?url=https%3A%2F%2Fassets.aboutamazon.com%2F3b%2Fe1%2F5c118a454aaeb1d7f9c12ca435f1%2F0039.JPG',
-        'https://assets.aboutamazon.com/dims4/default/249ee0f/2147483647/strip/true/crop/3533x2355%2B0%2B0/resize/1115x743%21/quality/90/?url=https%3A%2F%2Fassets.aboutamazon.com%2F1f%2F49%2Fa3850a084e678ca93a4986dac0ed%2F0040.JPG',
-        'https://assets.aboutamazon.com/dims4/default/aa92e69/2147483647/strip/true/crop/2000x1332%2B0%2B0/resize/1116x743%21/quality/90/?url=https%3A%2F%2Fassets.aboutamazon.com%2F74%2F83%2F553825b44e479fb263b2b1813c89%2Felectricvehicle-hollywood-9777-edited.JPG'
+        'https://s1.cdn.autoevolution.com/images/news/gallery/here-s-an-amazon-delivery-driver-s-opinion-about-his-brand-new-rivian-edv_2.jpg',
+        'https://hips.hearstapps.com/hmg-prod/images/img-8794-1669399662.jpg?crop=1.00xw%3A0.501xh%3B0%2C0.218xh',
+        'https://assets.aboutamazon.com/dims4/default/55a9346/2147483647/strip/true/crop/1600x900%2B0%2B0/resize/1320x743%21/quality/90/?url=https%3A%2F%2Fassets.aboutamazon.com%2Fdd%2Ffb%2Faee9e53c4d4eadb9906845d33b2f%2Famazon-delivery-driver-delivers-package.jpg'
       ]
     },
     {
       selector: '.fleet img',
       delay: 3000,
       images: [
-        'https://assets.aboutamazon.com/dims4/default/aa92e69/2147483647/strip/true/crop/2000x1332%2B0%2B0/resize/1116x743%21/quality/90/?url=https%3A%2F%2Fassets.aboutamazon.com%2F74%2F83%2F553825b44e479fb263b2b1813c89%2Felectricvehicle-hollywood-9777-edited.JPG',
-        'https://assets.aboutamazon.com/dims4/default/249ee0f/2147483647/strip/true/crop/3533x2355%2B0%2B0/resize/1115x743%21/quality/90/?url=https%3A%2F%2Fassets.aboutamazon.com%2F1f%2F49%2Fa3850a084e678ca93a4986dac0ed%2F0040.JPG',
-        'https://assets.aboutamazon.com/dims4/default/71cf205/2147483647/strip/true/crop/2000x1332%2B0%2B0/resize/1116x743%21/quality/90/?url=https%3A%2F%2Fassets.aboutamazon.com%2F07%2F4a%2Fdcb5351f463e9e0b9729ff86e435%2F0012.JPG'
+        'https://assets.aboutamazon.com/dims4/default/28827f7/2147483647/strip/true/crop/1600x900%2B0%2B0/resize/1320x743%21/quality/90/?url=https%3A%2F%2Fassets.aboutamazon.com%2F92%2Fd4%2F982db0da4638b03ea9d451901c4a%2Famazon-delivery-driver-unpacks-totes.jpg',
+        'https://assets.aboutamazon.com/dims4/default/24b936f/2147483647/strip/true/crop/1600x900%2B0%2B0/resize/1320x743%21/quality/90/?url=https%3A%2F%2Fassets.aboutamazon.com%2Fa2%2F98%2Fd2d13955446ab537ba11cf34ba63%2Fddd-inline-5.jpg',
+        'https://assets.aboutamazon.com/dims4/default/fe7c177/2147483647/strip/true/crop/1600x900%2B0%2B0/resize/1320x743%21/quality/90/?url=https%3A%2F%2Fassets.aboutamazon.com%2Fef%2Fc3%2F58137fa14c59adc1e6db89dbb852%2Fddd-inline-6.jpg'
       ]
     },
     {
       selector: '.career-hero > img',
       delay: 6000,
       images: [
-        'https://assets.aboutamazon.com/dims4/default/55a9346/2147483647/strip/true/crop/1600x900%2B0%2B0/resize/1320x743%21/quality/90/?url=https%3A%2F%2Fassets.aboutamazon.com%2Fdd%2Ffb%2Faee9e53c4d4eadb9906845d33b2f%2Famazon-delivery-driver-delivers-package.jpg',
-        'https://assets.aboutamazon.com/dims4/default/982f01e/2147483647/strip/true/crop/7099x4732%2B0%2B0/resize/1115x743%21/quality/90/?url=https%3A%2F%2Fassets.aboutamazon.com%2F8b%2F1e%2F296dec7c474fbaf4d2f3f4ab8b99%2F0004a.jpg',
-        'https://assets.aboutamazon.com/dims4/default/71cf205/2147483647/strip/true/crop/2000x1332%2B0%2B0/resize/1116x743%21/quality/90/?url=https%3A%2F%2Fassets.aboutamazon.com%2F07%2F4a%2Fdcb5351f463e9e0b9729ff86e435%2F0012.JPG'
+        'https://assets.aboutamazon.com/dims4/default/08ac5f8/2147483647/strip/true/crop/1600x900%2B0%2B0/resize/1320x743%21/quality/90/?url=https%3A%2F%2Fassets.aboutamazon.com%2Fea%2Fab%2F7099471345aebf2fca73562300bb%2Fddd-inline-7.jpg',
+        'https://assets.aboutamazon.com/dims4/default/de837ef/2147483647/strip/true/crop/1600x900%2B0%2B0/resize/1320x743%21/quality/90/?url=https%3A%2F%2Fassets.aboutamazon.com%2Fb1%2Fea%2F36a6de934c2f8c1d83a8eb4ca03e%2Famazon-delivery-driver-rivian-van.jpg',
+        'https://assets.aboutamazon.com/dims4/default/7fe5a6d/2147483647/strip/true/crop/1600x900%2B0%2B0/resize/1320x743%21/quality/90/?url=https%3A%2F%2Fassets.aboutamazon.com%2Ff7%2F74%2F8678870d4b13b73bbf3a36c918e5%2Fddd-inline-8.jpg'
       ]
     }
   ];
